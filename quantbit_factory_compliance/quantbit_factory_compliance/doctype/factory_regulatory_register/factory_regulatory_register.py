feef60febdd287905100ee97de5be31334053a1f
Sourabh Yelpale <sourabhyelpale29@gmail.com>
2026-01-19 13:09:53 +0530

    19 Jan 2026 - Task Doctype

--- quantbit_factory_compliance/quantbit_factory_compliance/doctype/factory_regulatory_register/factory_regulatory_register.py
+++ quantbit_factory_compliance/quantbit_factory_compliance/doctype/factory_regulatory_register/factory_regulatory_register.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2026, Quantbit Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FactoryRegulatoryRegister(Document):
-	pass
+    pass
+	# def before_save(self):
+	# 	due = self.due_day
+	# 	if(due < 1 or due  > 31):
+	# 		frappe.log_error("Enter date between 1-31")
