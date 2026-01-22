fb4cfe311eaa37a3c837bd905c0e5f64a91fb4b4
Sourabh Yelpale <sourabhyelpale29@gmail.com>
2026-01-22 14:32:28 +0530

    22 Jan 2026 - update Notifications

--- quantbit_factory_compliance/quantbit_factory_compliance/doctype/factory_regulatory_register/factory_regulatory_register.js
+++ quantbit_factory_compliance/quantbit_factory_compliance/doctype/factory_regulatory_register/factory_regulatory_register.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2026, Quantbit Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Factory Regulatory Register", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on("Factory Regulatory Register", {
+    setup(frm) {
+
+        frm.set_query("compliance", () => {
+            return {
+                filters: {
+                    disable: 0
+                }
+            };
+        });
+
+        frm.set_query("license", () => {
+            return {
+                filters: {
+                    disable: 0
+                }
+            };
+        });
+
+    }
+});
+
--- quantbit_factory_compliance/quantbit_factory_compliance/doctype/license/license.py
+++ quantbit_factory_compliance/quantbit_factory_compliance/doctype/license/license.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2026, Quantbit Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class License(Document):
 	pass
+
+def on_update(self, method=None):
+	if not self.disable:
+		return
+	else:
+		frappe.db.sql("""
+		UPDATE tabFactory Regulatory Register
+		SET license_status = 'Closed'
+		WHERE
+		category = 'License'
+		AND license = %s
+		AND docstatus = 1
+		""", (self.name,))
+		frappe.db.commit()
